045ba14cd7338b826db38c64bf57af4fb03c9757
amriteshonly4u <amriteshonly4u@gmail.com>
2019-03-25 17:03:37 +0530

    pushing

--- app/Http/Controllers/TestAPI/TestDetails_C.php
+++ app/Http/Controllers/TestAPI/TestDetails_C.php
@@ -10,5 +10,50 @@ class TestDetails_C extends Controller
 {
     public $successStatus = 200;
 	
-    //
+   
+
+// `test_info_id`,done
+// `test_name`, done
+// `test_price`, done
+// `status`, done
+// `pic`, done
+// `time_duration`, done
+// no of question 
+
+// `package_id`, 
+
+
+// `marks_on_correct`, 
+// `marks_on_incorrect`, 
+// `descrption`, 
+// `enroll_stud_count`, 
+// `expDate`, 
+// `created_at`, 
+// `updated_at`
+
+
+    //             NegativeMarking:'+2',
+    //             PositiveMarking:'-0.5',
+
+    public function TestDetails(Request $request){
+        // $testID = $request->json()->all()['testID'];
+    	$testID = 9;
+
+        $data = DB::table('test_info_tab')
+                  ->select(
+                    'descrption as descrption',
+                    // 'enroll_stud_count as enroll_stud_count',
+                    'marks_on_correct as PositiveMarking',
+                    'marks_on_incorrect as NegativeMarking',
+                    'expDate as expDate',
+                    'created_at as created_at'
+                  )
+                  ->where('test_info_id', '=', $testID)
+                  ->get();
+
+        $newData = $data[0]; //for one index value
+        return response()->json(['received'=>'yes','data'=>$newData,'return_test'=>$testID],$this->successStatus);
+    }
+
+
 }
--- app/Http/Controllers/TestAPI/TestList_C.php
+++ app/Http/Controllers/TestAPI/TestList_C.php
@@ -13,30 +13,19 @@ class TestList_C extends Controller
 
     public function TestList(Request $request){
         $subcatID = $request->json()->all()['subcategoryID'];
-        
-
-				// {
-    //               "info_id":"1",
-    //               "avtar_url":"https://instagram.fpat1-1.fna.fbcdn.net/vp/84c4e443d47dc2aa70a613a017a4c001/5CBB0AAC/t51.2885-19/s150x150/31908285_2109461939310314_4190149362170462208_n.jpg?_nc_ht=instagram.fpat1-1.fna.fbcdn.net",
-    //               "name":"Test 1",
-    //               "minutes":"50",
-    //               "":"1500",
-    //               "":"0"
-    //             },
-
-
-
+    	// $subcatID = 5;
 
         $data = DB::table('test_info_tab')
                   ->select(
-                    'test_info_id as info_id',
+                    'test_info_id as test_info_id',
                     'test_name as name',
                     'pic as avtar_url',
                     'time_duration as minutes',
                     'test_price as rate',
-                    DB::raw('COUNT(*) FROM `question_tab` WHERE test_info_id = info_id as noofquestion')
+                    DB::raw('(SELECT COUNT(*) FROM `question_tab` WHERE test_info_id = test_info_tab.test_info_id) as noofquestion')
                   )
                   ->where('package_id', '=', $subcatID)
+                  ->orderBy('test_name', 'ASC')
                   ->get();
 
 
