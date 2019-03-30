a4ef0996086120b97b058fce88833351bf0ddf25
amriteshonly4u <amriteshonly4u@gmail.com>
2019-03-30 18:36:52 +0530

    pushign

--- resources/views/Website/index.blade.php
+++ resources/views/Website/index.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Hardigurdi (self Belive) | A Online Test Series</title>
+  <title>Hardigurdi (Self Belief) | A Online Test Series</title>
   
   <!-- PLUGINS CSS STYLE -->
   <!-- Bootstrap -->
