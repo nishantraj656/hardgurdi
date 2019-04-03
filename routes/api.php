74ded9cef27eaf047823d96165be4ecf0624e1a6
amriteshonly4u <amriteshonly4u@gmail.com>
2019-04-03 09:49:55 +0530

    AIR ADDED

--- routes/api.php
+++ routes/api.php
@@ -85,7 +85,7 @@ Route::post('change_password_HD', 'TestAPI\LoginSignUP@change_password_fun');
 
 Route::get('test', 'TestAPI\HistoryList_C@HistList');
 Route::get('test1', 'TestAPI\PaymentHist@getPayHist');
-Route::get('test3', 'TestAPI\TestList_C@purchasedTestList');
+Route::get('test3', 'TestAPI\ResultController@getAIR');
 
 
 
